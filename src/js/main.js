9416f5ad2e90fa282d1efb95c845e3175c4be561
Vadim <vadimiskakov123work@gmail.com>
2023-06-03 00:24:22 +0500

    the end

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,14 @@ function toggleMenu() {
     const burger = document.querySelector('#burger')
     const menu = document.querySelector('#menu')
     const body = document.querySelector('body')
+    const navbtns = document.querySelector('#btns')
         burger.addEventListener('click',()=>{
             burger.classList.toggle('active')
             menu.classList.toggle('hidden')
             menu.classList.toggle('flex')
             body.classList.toggle('overflow-hidden')
+            btns.classList.toggle('hidden')
+            btns.classList.toggle('flex')
         })
         window.addEventListener('resize',()=>{
             if(window.innerWidth > 767.99) {
@@ -14,6 +17,8 @@ function toggleMenu() {
                 menu.classList.remove('flex')
                 burger.classList.remove('active')
                 body.classList.remove('overflow-hidden')
+                btns.classList.add('hidden')
+                btns.classList.remove('flex')
             }
         })
 }
